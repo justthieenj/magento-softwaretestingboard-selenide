fc52b4066a3dead5013772e71165e60f9ba446b9
Thien Nguyen <ngkhanhthien94@gmail.com>
2023-05-30 19:36:37 +0700

    impl parallel execution

--- src/test/java/cucumbertest/AllCucumberTest.java
+++ src/test/java/cucumbertest/AllCucumberTest.java
@@ -2,11 +2,17 @@ package cucumbertest;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = "src/test/resources/features/",
+        features = "src/test/resources/features/search-and-filter.feature",
         glue = "stepdefs",
         plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", "pretty"}
 )
 public class AllCucumberTest extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
--- src/test/java/stepdefs/Hook.java
+++ src/test/java/stepdefs/Hook.java
@@ -1,21 +1,26 @@
 package stepdefs;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.qameta.allure.selenide.AllureSelenide;
 
 public class Hook {
     @Before
     public void beforeScenario() {
-        System.out.println("before");
         configSelenide();
+    }
+
+    @After
+    public void afterScenario() {
+        Selenide.closeWebDriver();
     }
 
     private void configSelenide() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.headless = false;
         Configuration.timeout = 10000;
-//        Configuration.pageLoadTimeout;
     }
 }
